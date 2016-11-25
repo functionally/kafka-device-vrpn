f5be6fa90ab995fe1b63777593216aca971cd9e4
Brian W Bush <b.w.bush@acm.org>
2016-11-24 20:00:32 -0700

    more work in progress

--- src/Network/UI/Kafka/VRPN.hs
+++ src/Network/UI/Kafka/VRPN.hs
@@ -20,8 +20,39 @@ import Network.Kafka (KafkaAddress, KafkaClientId)
 import Network.Kafka.Protocol (TopicName)
 import Network.UI.Kafka (ExitAction, LoopAction, Sensor, producerLoop)
 import Network.UI.Kafka.Types (Event(..))
-import Network.VRPN
+
+import qualified Network.VRPN as V
 
 
-vrpnLoop :: String -> KafkaClientId -> KafkaAddress -> TopicName -> Sensor -> IO (ExitAction, LoopAction)
-vrpnLoop = undefined
+data VrpnCallback =
+    Tracker
+  | Button
+  | Analog
+  | Dial
+    deriving (Bounded, Enum, Eq, Ord, Read, Show)
+
+
+vrpnLoop :: String -> KafkaClientId -> KafkaAddress -> TopicName -> Sensor -> [VrpnCallbacks] -> IO (ExitAction, LoopAction)
+vrpnLoop device client address topic sensor callback s=
+  do
+    nextEvent <- newEmptyMVar
+    devices <-
+      mapM V.openDevice
+        $ filter ((`elem` callbacks) . fst)
+        [
+          (Tracker, V.Tracker device (Just $ positionCallback nextEvent) Nothing Nothing)
+        , (Button , V.Button  device (Just $ buttonCallback   nextEvent)                )
+        , (Analog , V.Analog  device (Just $ analogCallback   nextEvent)                )
+        , (Dial   , V.Dial    device (Just $ dialCallback     nextEvent)                )
+        ]
+    undefined
+
+
+positionCallback :: MVar Event -> V.PositionCallback
+positionCallback nextEvent _ s (px, py, pz) (ox, oy, oz) =
+  undefined
+
+
+buttonCallback :: MVar Event -> V.ButtonCallback
+buttonCallback nextEvent _ s :qa
+
