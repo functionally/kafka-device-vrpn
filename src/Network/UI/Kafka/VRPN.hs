99ea3131bd73fff737f8a51531707ab43950521f
Brian W Bush <b.w.bush@acm.org>
2016-11-24 19:59:18 -0700

    work in progress

--- src/Network/UI/Kafka/VRPN.hs
+++ src/Network/UI/Kafka/VRPN.hs
@@ -1,5 +1,5 @@
 {-|
-Module      :  Main
+Module      :  Network.UI.Kafka.VRPN
 Copyright   :  (c) 2016 Brian W Bush
 License     :  MIT
 Maintainer  :  Brian W Bush <consult@brianwbush.info>
@@ -12,4 +12,16 @@ Produce events on a Kafka topic from VRPN \<<https://github.com/vrpn/vrpn/wiki>\
 
 module Network.UI.Kafka.VRPN (
 -- * Event loop
+  vrpnLoop
 ) where
+
+
+import Network.Kafka (KafkaAddress, KafkaClientId)
+import Network.Kafka.Protocol (TopicName)
+import Network.UI.Kafka (ExitAction, LoopAction, Sensor, producerLoop)
+import Network.UI.Kafka.Types (Event(..))
+import Network.VRPN
+
+
+vrpnLoop :: String -> KafkaClientId -> KafkaAddress -> TopicName -> Sensor -> IO (ExitAction, LoopAction)
+vrpnLoop = undefined
