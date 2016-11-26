2b13518ad8dd92577e644548fedb1e866de62b6a
Brian W Bush <b.w.bush@acm.org>
2016-11-26 07:46:36 -0700

    first working version

--- src/Main.hs
+++ src/Main.hs
@@ -16,6 +16,35 @@ module Main (
 ) where
 
 
+import Data.String (fromString)
+import Network.UI.Kafka.VRPN (vrpnLoop)
+import System.Environment (getArgs)
+
+
 -- | The main action.
 main :: IO ()
-main = undefined
+main =
+  do
+    args <- getArgs
+    case args of
+      [device, client, host, port, topic, sensor] ->
+        do
+          let
+            callbacks = [minBound..maxBound]
+          putStrLn $ "VRPN device:    " ++ device
+          putStrLn $ "Kafka client:   " ++ client
+          putStrLn $ "Kafka address:  (" ++ host ++ "," ++ port ++ ")"
+          putStrLn $ "Kafka topic:    " ++ topic
+          putStrLn $ "Sensor name:    " ++ sensor
+          putStrLn $ "GLUT callbacks: " ++ show callbacks
+          (_, loop) <-
+            vrpnLoop
+              device
+              (fromString client)
+              (fromString host, toEnum $ read port)
+              (fromString topic)
+              sensor
+              callbacks
+          result <- loop
+          either print return result
+      _ -> putStrLn "USAGE: kafka-device-vrpn device client host port topic senosr"
--- src/Network/UI/Kafka/VRPN.hs
+++ src/Network/UI/Kafka/VRPN.hs
@@ -16,28 +16,43 @@ module Network.UI.Kafka.VRPN (
 ) where
 
 
+import Control.Concurrent (MVar, forkIO, isEmptyMVar, newEmptyMVar, putMVar, takeMVar)
+import Control.Monad (void, zipWithM_)
 import Network.Kafka (KafkaAddress, KafkaClientId)
 import Network.Kafka.Protocol (TopicName)
 import Network.UI.Kafka (ExitAction, LoopAction, Sensor, producerLoop)
-import Network.UI.Kafka.Types (Event(..))
+import Network.UI.Kafka.Types (Button(..), Event(..))
 
 import qualified Network.VRPN as V
 
 
+-- | Types of VRPN callbacks.  See \<<https://hackage.haskell.org/package/vrpn/docs/Network-VRPN.html>\> for more details.
 data VrpnCallback =
+    -- | Change in tracker state.
     Tracker
-  | Button
-  | Analog
-  | Dial
+  | -- | Change in button state.
+    Button
+  | -- | Change in analog axis state.
+    Analog
+  | -- | Change in dial state.
+    Dial
     deriving (Bounded, Enum, Eq, Ord, Read, Show)
 
 
-vrpnLoop :: String -> KafkaClientId -> KafkaAddress -> TopicName -> Sensor -> [VrpnCallbacks] -> IO (ExitAction, LoopAction)
-vrpnLoop device client address topic sensor callback s=
+-- | Produce events for a Kafka topic from VRPN callbacks \<<https://hackage.haskell.org/package/vrpn/docs/Network-VRPN.html>\>.
+vrpnLoop :: String                      -- ^ The VRPN host, e.g. spacenav0@localhost.
+         -> KafkaClientId               -- ^ A Kafka client identifier for the producer.
+         -> KafkaAddress                -- ^ The address of the Kafka broker.
+         -> TopicName                   -- ^ The Kafka topic name.
+         -> Sensor                      -- ^ The name of the sensor producing events.
+         -> [VrpnCallback]              -- ^ Which callbacks to enable.
+         -> IO (ExitAction, LoopAction) -- ^ Action to create the exit and loop actions.
+vrpnLoop device clientId address topic sensor callbacks =
   do
+    exitNow <- newEmptyMVar
     nextEvent <- newEmptyMVar
     devices <-
-      mapM V.openDevice
+      mapM (V.openDevice . snd)
         $ filter ((`elem` callbacks) . fst)
         [
           (Tracker, V.Tracker device (Just $ positionCallback nextEvent) Nothing Nothing)
@@ -45,14 +60,54 @@ vrpnLoop device client address topic sensor callback s=
         , (Analog , V.Analog  device (Just $ analogCallback   nextEvent)                )
         , (Dial   , V.Dial    device (Just $ dialCallback     nextEvent)                )
         ]
-    undefined
+    (exit, loop) <-
+      producerLoop clientId address topic sensor
+      $ (: [])
+      <$> takeMVar nextEvent
+    return
+      (
+        do
+          putMVar exitNow ()
+          exit
+      , do
+          void
+            . forkIO
+            $ V.mainLoops (not <$> isEmptyMVar exitNow) (1 :: Double) devices
+          loop
+      )
 
 
-positionCallback :: MVar Event -> V.PositionCallback
-positionCallback nextEvent _ s (px, py, pz) (ox, oy, oz) =
-  undefined
+-- | Translate VRPN position state change to events.
+positionCallback :: MVar Event                    -- ^ Reference to the next event.
+                 -> V.PositionCallback Int Double -- ^ The VRPN callback.
+positionCallback nextEvent _ _ p o =
+  do
+    putMVar nextEvent
+      $ LocationEvent p
+    putMVar nextEvent
+      $ OrientationEvent o
 
 
-buttonCallback :: MVar Event -> V.ButtonCallback
-buttonCallback nextEvent _ s :qa
+-- | Translate VRPN button state change to events. 
+buttonCallback :: MVar Event           -- ^ Reference to the next event.
+               -> V.ButtonCallback Int -- ^ The VRPN callback.
+buttonCallback nextEvent _ i x =
+  putMVar nextEvent
+    $ ButtonEvent (IndexButton i, toEnum $ fromEnum $ not x) 
 
+
+-- | Translate VRPN analog state change to events.
+analogCallback :: MVar Event              -- ^ Reference to the next event.
+               -> V.AnalogCallback Double -- ^ The VRPN callback.
+analogCallback nextEvent _ =
+  zipWithM_
+    ((putMVar nextEvent .) . AnalogEvent)
+    [0..]
+
+
+-- | Translate VRPN dial state change to events.
+dialCallback :: MVar Event                -- ^ Reference to the next event.
+             -> V.DialCallback Int Double -- ^ The VRPN callback.
+dialCallback nextEvent _ i x =
+  putMVar nextEvent
+    $ DialEvent i x
